21de03ecbeb0e969cd17082441a8c33a59c499db
Bohdan Tkachenko <bogus.weber@gmail.com>
2015-06-06 00:23:44 +0300

    Implement ES6 import syntax

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 This repository will give access to new rules for the ESLint tool. You should use it only if you are developing a CommonJS application. It checks for require() function usage.
 
+# Features
+- Supports both `require()` and ES6 `import` syntax
+- Supports aliases in webpack
+- Supports different file extensions
+- Works in Atom with `linter-eslint` package
+
 # Usage
 
 1. Install `eslint-plugin-require-path-exists` as a dev-dependency:
@@ -32,3 +38,14 @@ This repository will give access to new rules for the ESLint tool. You should us
 | exists  | You should only pass existent paths to require() | 'exists': 2 |
 | not_empty | You should not call require() without arguments or with empty argument | 'not_empty': 2 |
 | too_many_arguments | You should pass only one argument to require() function | 'too_many_arguments': 2 |
+
+# ToDo
+
+- Tests coverage.
+- Check in different CommonJS environments (currently only tested in NodeJS and webpack).
+- Remove dirty hack for Atom after [pull request](https://github.com/AtomLinter/linter-eslint/pull/89) in `linter-eslint` is merged.
+- Think about more correct way to detect bundled NodeJS (and webpack) modules. Currently it is a constant array.
+
+# License
+
+MIT
--- rules/exists.js
+++ rules/exists.js
@@ -3,6 +3,8 @@
 var fs = require('fs-plus');
 var path = require('path');
 var url = require('url');
+
+// TODO: any more correct way to do this?
 var BUNDLED_MODULES = [
   'assert', 'buffer', 'child_process', 'cluster', 'console', 'constants', 'crypto', 'dgram', 'dns', 'domain', 'events',
   'freelist', 'fs', 'http', 'https', 'module', 'net', 'os', 'path', 'punycode', 'querystring', 'readline', 'repl',
@@ -30,56 +32,50 @@ function getModulesDir(fromDir) {
   return null;
 }
 
-function resolveModule(fromDir, modulesDir) {
-  return function (value) {
-    var pathname = url.parse(value).pathname;
+function resolveModule(value, fromDir, modulesDir) {
+  var pathname = url.parse(value).pathname;
 
-    if (pathname[0] === '.') { // relative
-      return path.join(fromDir, pathname);
-    } else if (pathname[0] === '/') { // absolute
-      return value;
-    } else if (modulesDir) { // node_modules
-      var moduleDir = path.join(modulesDir, value);
-      var packageFilename = path.join(moduleDir, 'package.json');
+  if (pathname[0] === '.') { // relative
+    return path.join(fromDir, pathname);
+  } else if (pathname[0] === '/') { // absolute
+    return value;
+  } else if (modulesDir) { // node_modules
+    var moduleDir = path.join(modulesDir, value);
+    var packageFilename = path.join(moduleDir, 'package.json');
 
-      if (fs.existsSync(packageFilename)) {
-        var pkg;
+    if (fs.existsSync(packageFilename)) {
+      var pkg;
 
-        try {
-          pkg = JSON.parse(fs.readFileSync(packageFilename));
-        } catch (e) {
-          pkg = false;
-        }
-
-        if (pkg && pkg.main && !Array.isArray(pkg.main) && pkg.main !== 'index.js') {
-          return path.join(moduleDir, pkg.main);
-        }
+      try {
+        pkg = JSON.parse(fs.readFileSync(packageFilename));
+      } catch (e) {
+        pkg = false;
       }
 
-      return moduleDir;
+      if (pkg && pkg.main && !Array.isArray(pkg.main) && pkg.main !== 'index.js') {
+        return path.join(moduleDir, pkg.main);
+      }
     }
 
-    return value;
-  };
+    return moduleDir;
+  }
 }
 
-function checkPath(extensions) {
-  return function (pathname) {
-    pathname = fs.resolveExtension(pathname, extensions);
+function checkPath(pathname, extensions) {
+  pathname = fs.resolveExtension(pathname, extensions);
 
-    if (!pathname) {
-      return true;
-    }
-
-    if (fs.isDirectorySync(pathname)) {
-      pathname = fs.resolveExtension(path.join(pathname, 'index'), extensions);
-      if (!pathname) {
-        return true;
-      }
-    }
-
+  if (!pathname) {
     return false;
-  };
+  }
+
+  if (fs.isDirectorySync(pathname)) {
+    pathname = fs.resolveExtension(path.join(pathname, 'index'), extensions);
+    if (!pathname) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function isAtom() {
@@ -128,54 +124,67 @@ function getWebpackConfig(fromDir) {
   return {};
 }
 
+function testModulePath(value, context, node) {
+  if (BUNDLED_MODULES.indexOf(value) >= 0) {
+    return;
+  }
+
+  var fileDir = getCurrentFilePath(context);
+  if (!fileDir) {
+    return;
+  }
+
+  var modulesDir = getModulesDir(fileDir) || '';
+  var webpackConfig = getWebpackConfig(fileDir);
+  var alias = {};
+  var extensions = Object.keys(require.extensions);
+
+  if (extensions.indexOf('') === -1) {
+    extensions.push('');
+  }
+
+  if (typeof webpackConfig.resolve === 'object') {
+    if (typeof webpackConfig.resolve.alias === 'object') {
+      alias = webpackConfig.resolve.alias;
+    }
+
+    if (Array.isArray(webpackConfig.resolve.extensions)) {
+      webpackConfig.resolve.extensions.forEach(function (ext) {
+        if (extensions.indexOf(ext) === -1) {
+          extensions.unshift(ext);
+        }
+      });
+    }
+  }
+
+  value = alias[value] ? alias[value] : value;
+  value = resolveModule(value, fileDir, modulesDir);
+
+  if (checkPath(value, extensions)) {
+    return;
+  }
+
+  value = value.replace(fileDir + (/\/$/.test(fileDir) ? '' : path.sep), './');
+  value = value.replace(modulesDir + (/\/$/.test(modulesDir) ? '' : path.sep), '');
+
+  context.report(node, "Cannot find module '" + value + "'", {});
+}
+
 module.exports = function (context) {
   return {
+    ImportDeclaration: function (node) {
+      console.log(node.source.value);
+
+      testModulePath(node.source.value, context, node);
+    },
     CallExpression: function (node) {
       if (node.callee.name !== 'require' || !node.arguments.length || typeof node.arguments[0].value !== 'string' || !node.arguments[0].value) {
         return;
       }
 
-      var fileDir = getCurrentFilePath(context);
-      if (!fileDir) {
-        return;
-      }
-
-      var modulesDir = getModulesDir(fileDir) || '';
-      var webpackConfig = getWebpackConfig(fileDir);
-      var alias = {};
-      var extensions = Object.keys(require.extensions);
-
-      if (extensions.indexOf('') === -1) {
-        extensions.push('');
-      }
-
-      if (typeof webpackConfig.resolve === 'object') {
-        if (typeof webpackConfig.resolve.alias === 'object') {
-          alias = webpackConfig.resolve.alias;
-        }
-
-        if (Array.isArray(webpackConfig.resolve.extensions)) {
-          webpackConfig.resolve.extensions.forEach(function (ext) {
-            if (extensions.indexOf(ext) === -1) {
-              extensions.push(ext);
-            }
-          });
-        }
-      }
-
-      node.arguments[0].value.split('!')
-        .filter(function (value) {
-          return BUNDLED_MODULES.indexOf(value) === -1;
-        })
-        .map(function (item) { return alias[item] ? alias[item] : item; })
-        .map(resolveModule(fileDir, modulesDir))
-        .filter(checkPath(extensions))
-        .forEach(function (pathname) {
-          pathname = pathname.replace(fileDir + (/\/$/.test(fileDir) ? '' : path.sep), './');
-          pathname = pathname.replace(modulesDir + (/\/$/.test(modulesDir) ? '' : path.sep), '');
-
-          context.report(node, "Cannot find module '" + pathname + "'", {});
-        });
+      node.arguments[0].value.split('!').forEach(function (value) {
+        testModulePath(value, context, node);
+      });
     }
   };
 };
